148f61894c69398ddf553c47652c4ac29ad3d4a4
Arvind Kathare <arvind.kathare@thevouch.digital>
2024-10-07 09:50:10 +0530

    updated code

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ cloudinary.config({
 
 
 
+const PORT = process.env.PORT || 4000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
