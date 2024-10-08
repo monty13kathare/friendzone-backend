18f8ecff45a98e7e6a24bf1deca53f0d244763ad
Arvind Kathare <arvind.kathare@thevouch.digital>
2024-10-08 10:12:09 +0530

    code updated

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "config/config.env" });
 }
 
-// CORS options to allow requests from the frontend (http://localhost:3000)
 const corsOptions = {
-  origin: 'http://localhost:3000',  // Replace with your frontend URL
-  credentials: true,  // Allow cookies to be sent
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",  // Allowed methods
-  allowedHeaders: "Content-Type,Authorization",  // Allowed headers
-  optionsSuccessStatus: 200  // For legacy browser support
+  origin:"http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
 };
+
 
 // Apply CORS middleware to handle cross-origin requests
 app.use(cors(corsOptions));
