ac8688b9093118f37c854145ea92607effb100fd
Arvind Kathare <arvind.kathare@thevouch.digital>
2024-10-08 10:29:28 +0530

    updated

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const corsOptions = {
-  origin:["http://localhost:3000", "https://friend-zone-eahnr3bl9-arvind-kathares-projects.vercel.app"],
+  origin:["http://localhost:3000"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
