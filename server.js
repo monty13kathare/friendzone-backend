bc021075503e91ee80939b5e3962e3f40917d81b
Arvind Kathare <arvind.kathare@thevouch.digital>
2024-10-13 10:06:40 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ cloudinary.config({
 
 const PORT = process.env.PORT || 4000;
 
+app.get("/",(req,res)=>{
+   res.send({message:"Api is working.."})
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
