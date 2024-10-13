a384a4f55814deddf1a2793ed643b013b0d0311f
Arvind Kathare <arvind.kathare@thevouch.digital>
2024-10-13 09:54:46 +0530

    re-initialise

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = () => {
   try {
-    mongoose
-    .connect(process.env.MONGO_URI)
+    mongoose.set('strictQuery', false);
+    mongoose.connect(process.env.MONGO_URI)
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
   } catch (error) {
